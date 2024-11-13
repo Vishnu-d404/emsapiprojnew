d88c34e2c5cb7740e347e1b248fcdd47b1170761
Vishnu-d404 <vishnud3966@gmail.com>
2024-11-13 16:57:42 +0530

    Update models.py by some another guy

--- apibackendapp/models.py
+++ apibackendapp/models.py
@@ -4,7 +4,7 @@ from django.db.models.signals import post_save
 from django.conf import settings
 from rest_framework.authtoken.models import Token
 
-# Create your models here.
+# Create your amazing models here.
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
